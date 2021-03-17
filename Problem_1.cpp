5ede397a7ff52074be5e38a90b46b09ebdf966b9
Luke Eltiste <luke@ilsautomation.com>
2021-03-17 08:54:31 -0700

    Add project files.

---
+++ Problem_1.cpp
@@ -0,0 +1,30 @@
+/*
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+*/
+
+#include <vector>
+#include <iostream>
+
+bool arrayAddToNum(std::vector<int> arr, int k) {
+	for (auto i = 0; i < arr.size() - 1; ++i) {
+		for (auto j = i; j < arr.size(); ++j) {
+			if (arr[i] + arr[j] == k) return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	std::string str = (arrayAddToNum({ 10,15,3,7 }, 17)) ? "True" : "False";
+
+	std::cout << str << std::endl;
+
+	str = (arrayAddToNum({ 1,5,7,9 }, 11)) ? "True" : "False";
+	std::cout << str << std::endl;
+
+	return 0;
+}
