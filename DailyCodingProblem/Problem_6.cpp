6878c0b2f9695c9d6dacd871d46d7b787dfbc9e9
Luke Eltiste <luke@ilsautomation.com>
2021-03-24 13:10:56 -0700

    Problem 6 - Followed Solution

--- DailyCodingProblem/Problem_6.cpp
+++ DailyCodingProblem/Problem_6.cpp
@@ -8,19 +8,90 @@ If using a language that has no pointers (such as Python), you can assume you ha
 */
 #include <iostream>
 
+struct xNode
+{
+  int     val;
+  xNode * both;
+
+  xNode( int value )
+  {
+    val  = value;
+    both = NULL;
+  }
+};
+
+xNode * xorNode( xNode * a, xNode * b )
+{
+  return (xNode *)( (uintptr_t)a ^ (uintptr_t)b );
+}
+
 class xor_list
 {
 public:
-  xor_list() {};
+  xNode * head;
+  xNode * tail;
 
-  void add( int x ) {};
+  xor_list()
+  {
+    head = NULL;
+    tail = NULL;
+  }
 
-  int get( int i ) { return 0; };
+  void add( xNode * node )
+  {
+    if( head == NULL )
+    {
+      head = node;
+      tail = node;
+    }
+    else
+    {
+      tail->both = xorNode( node, tail->both );
+      node->both = tail;
+      tail       = node;
+    }
+  }
+
+  xNode * get( int index )
+  {
+    xNode * prev_id = NULL;
+    xNode * node    = head;
+    for( int i = 0; i < index; ++i )
+    {
+      xNode * next_id = xorNode( prev_id, node->both );
+
+      if( next_id )
+      {
+        prev_id = node;
+        node    = next_id;
+      }
+      else
+      {
+        throw "Linked List index out of range";
+      }
+    }
+    return node;
+  };
 };
 
+void printAtIndex( xor_list list, int index )
+{
+  std::cout << "The index " << index << " contains the value " << list.get( index )->val << '\n';
+}
+
 int prob_6()
-{ // TODO IDK how to do this
+{
   std::cout << "\nProblem 6:\n";
-  std::cout << "Skipped because IDK how\n";
+
+  xor_list list;
+
+  list.add( new xNode( 5 ) );
+  list.add( new xNode( 3 ) );
+  list.add( new xNode( 18 ) );
+
+  printAtIndex( list, 0 );
+  printAtIndex( list, 1 );
+  printAtIndex( list, 2 );
+
   return 0;
 }
--- DailyCodingProblem/Problem_6.h
+++ DailyCodingProblem/Problem_6.h
@@ -1,3 +1,8 @@
 #pragma once
 
+struct xNode;
+xNode * xorNode( xNode * a, xNode * b );
+
+class xor_list;
+
 int prob_6();
