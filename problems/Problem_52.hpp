5e01dedbd0a6555311586ae44b3dd6f4f9502bf4
Luke Eltiste <luke343279@gmail.com>
2021-05-07 12:11:09 -0700

    Problem 52 - Used Solution

--- problems/Problem_52.hpp
+++ problems/Problem_52.hpp
@@ -10,3 +10,96 @@ should also remove the least recently used item.
 
 Each operation should run in O(1) time.
 */
+
+#include <map>
+#include <string>
+
+struct LLNode
+{
+  std::string key;
+  std::string val;
+  LLNode *    prev;
+  LLNode *    next;
+
+  LLNode( std::string _key, std::string _val )
+  {
+    key  = _key;
+    val  = _val;
+    prev = NULL;
+    next = NULL;
+  }
+};
+
+struct LinkedList
+{
+  LLNode * head;
+  LLNode * tail;
+
+  LinkedList()
+  {
+    // dummy nodes
+    head = new LLNode( "", "head" );
+    tail = new LLNode( "", "tail" );
+    // set up head <-> tail
+    head->next = tail;
+    tail->prev = head;
+  }
+
+  LLNode * get_head() { return head->next; }
+
+  LLNode * get_tail() { return tail->prev; }
+
+  void add( LLNode * node )
+  {
+    LLNode * prev = tail->prev;
+    prev->next    = node;
+    node->prev    = prev;
+    node->next    = tail;
+    tail->prev    = node;
+  }
+
+  void remove( LLNode * node )
+  {
+    LLNode * prev = node->prev;
+    LLNode * nxt  = node->next;
+    prev->next    = nxt;
+    nxt->prev     = prev;
+  }
+};
+
+struct LRUCache
+{
+  int                             n;
+  std::map<std::string, LLNode *> dict;
+  LinkedList                      list;
+
+  LRUCache( int _n ) { n = _n; }
+
+  void set( std::string key, std::string val )
+  {
+    if( dict.find( key ) != dict.end() ) { dict.erase( key ); }
+
+    LLNode * node = new LLNode( key, val );
+    list.add( node );
+    dict[key] = node;
+    if( dict.size() > n )
+    {
+      LLNode * head = list.get_head();
+      list.remove( head );
+      dict.erase( head->key );
+    }
+  }
+
+  std::string get( std::string key )
+  {
+    if( dict.find( key ) != dict.end() )
+    {
+      LLNode * node = dict[key];
+      // bump to the back of the list by removing and adding the node
+      list.remove( node );
+      list.add( node );
+      return node->val;
+    }
+    return "";
+  }
+};
--- src/test_Problems_51_60.cpp
+++ src/test_Problems_51_60.cpp
@@ -10,3 +10,32 @@ TEST( Problem_51, Test_Case )
 
   auto shuffled = shuffle( arr );
 }
+
+// Problem 52
+LRUCache cache( 5 );
+TEST( Problem_52, set )
+{
+  EXPECT_NO_THROW( cache.set( "one", "1" ) );
+  EXPECT_NO_THROW( cache.set( "two", "2" ) );
+  EXPECT_NO_THROW( cache.set( "three", "3" ) );
+  EXPECT_NO_THROW( cache.set( "four", "4" ) );
+  EXPECT_NO_THROW( cache.set( "five", "5" ) );
+  EXPECT_NO_THROW( cache.set( "six", "6" ) );
+}
+
+TEST( Problem_52, get )
+{
+  EXPECT_EQ( cache.get( "one" ), "" );
+  EXPECT_EQ( cache.get( "two" ), "2" );
+  EXPECT_EQ( cache.get( "three" ), "3" );
+  EXPECT_EQ( cache.get( "four" ), "4" );
+  EXPECT_EQ( cache.get( "five" ), "5" );
+  EXPECT_EQ( cache.get( "six" ), "6" );
+}
+
+TEST( Problem_52, CacheSizeTest )
+{
+  cache.set( "seven", "7" );
+  EXPECT_EQ( cache.get( "seven" ), "7" );
+  EXPECT_EQ( cache.get( "two" ), "" );
+}
