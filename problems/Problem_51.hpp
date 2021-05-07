17554598ee1b86ffa7820b1b658da83dd55bd73f
Luke Eltiste <luke343279@gmail.com>
2021-05-07 11:31:23 -0700

    Problem 51

--- problems/Problem_51.hpp
+++ problems/Problem_51.hpp
@@ -8,3 +8,19 @@ It should run in O(N) time.
 
 Hint: Make sure each one of the 52! permutations of the deck is equally likely.
 */
+
+#include <vector>
+
+std::vector<int> shuffle( std::vector<int> arr )
+{
+  int n = arr.size();
+
+  for( int i = 0; i < n - 1; i++ )
+  {
+    int j    = rand() % ( n - 1 - i ) + i;
+    int temp = arr[i];
+    arr[i]   = arr[j];
+    arr[j]   = temp;
+  }
+  return arr;
+}
--- src/test_Problems_51_60.cpp
+++ src/test_Problems_51_60.cpp
@@ -2,3 +2,11 @@
 #include "Problem_52.hpp"
 
 #include "gtest/gtest.h"
+
+// Problem 51
+TEST( Problem_51, Test_Case )
+{
+  std::vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  auto shuffled = shuffle( arr );
+}
