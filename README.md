cd6b837cf967c115b0330375b426ccbdc30ae00b
Luke Eltiste <luke343279@gmail.com>
2021-06-01 10:29:52 -0700

    add problem 78

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ My Solutions to the [Daily Coding Problem](https://www.dailycodingproblem.com/)
 - Problem 59
 - Problem 76
 - Problem 77
+- Problem 78
---
+++ problems/Problem_078.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+/* MEDIUM
+Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list.
+*/
