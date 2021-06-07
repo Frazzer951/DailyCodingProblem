e2caa0d0d861ee1bb63397699b226dc080e39681
Luke Eltiste <luke343279@gmail.com>
2021-06-07 09:14:06 -0700

    problem 82

--- src/test_Problems_081_090.cpp
+++ src/test_Problems_081_090.cpp
@@ -1,4 +1,7 @@
 #include "Problem_081.hpp"
+#include "Problem_082.hpp"
+#include "Problem_083.hpp"
+#include "Problem_084.hpp"
 
 #include "gtest/gtest.h"
 
@@ -18,4 +21,32 @@ TEST( Problem_81, Given_Case )
   std::vector<std::string> result   = num_to_str( "23", num_map );
 
   EXPECT_EQ( expected, result );
+}
+
+// Problem 82
+TEST( Problem_82, Test_read7 )
+{
+  filereader filereader( "Problem_082.txt" );
+
+  std::string one   = filereader.read7();
+  std::string two   = filereader.read7();
+  std::string three = filereader.read7();
+
+  EXPECT_EQ( "Hello w", one );
+  EXPECT_EQ( "orld", two );
+  EXPECT_EQ( "", three );
+}
+
+TEST( Problem_82, Test_Case_1 )
+{
+  std::string result = readN( 5 );
+
+  EXPECT_EQ( "Hello", result );
+}
+
+TEST( Problem_82, Test_Case_2 )
+{
+  std::string result = readN( 11 );
+
+  EXPECT_EQ( "Hello world", result );
 }
