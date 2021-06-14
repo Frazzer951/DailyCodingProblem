58c4015b092f2626859c52e472fdfc62f7164a5a
Luke Eltiste <luke343279@gmail.com>
2021-06-14 09:02:40 -0700

    add quicker division

--- problems/Problem_088.hpp
+++ problems/Problem_088.hpp
@@ -6,6 +6,7 @@ multiplication, or modulus operators. Return the quotient as an integer,
 ignoring the remainder.
 */
 
+/*
 inline int divide( const int & x, const int & y )
 {
   int i       = 0;
@@ -18,4 +19,27 @@ inline int divide( const int & x, const int & y )
   }
 
   return i;
+}
+*/
+
+inline int divide( int x, int y )
+{
+  if( y == 0 )
+    return -1;
+
+  int quotient  = 0;
+  int power     = 32;            // Assume 32-bit integer
+  int yPower    = y << power;    // Initial y^d value is y^32
+  int remainder = x;             // Initial remainder is x
+  while( remainder >= y )
+  {
+    while( yPower > remainder )
+    {
+      yPower >>= 1;
+      power -= 1;
+    }
+    quotient += 1 << power;
+    remainder -= yPower;
+  }
+  return quotient;
 }
