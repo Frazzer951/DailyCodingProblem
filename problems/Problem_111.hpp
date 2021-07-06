1c63721609acd1cc14f64fd821f6c1fc0d27bfdd
Luke Eltiste <luke343279@gmail.com>
2021-07-06 08:38:11 -0700

    Problem 111

--- problems/Problem_111.hpp
+++ problems/Problem_111.hpp
@@ -5,3 +5,38 @@ Given a word W and a string S, find all starting indices in S which are anagrams
 
 For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
 */
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+inline bool isAnagram( const std::string& s1, const std::string& s2 )
+{
+  std::map<char, int> count1;
+  std::map<char, int> count2;
+  std::set<char>      chars1;
+  std::set<char>      chars2;
+  for( char c : s1 )
+  {
+    count1[c]++;
+    chars1.insert( c );
+  }
+  for( char c : s2 )
+  {
+    count2[c]++;
+    chars2.insert( c );
+  }
+  if( chars1 != chars2 ) return false;
+  return count1 == count2;
+}
+
+inline std::vector<int> anagramIndex( const std::string& w, const std::string& s )
+{
+  std::vector<int> indecies;
+  for( int i = 0; i <= s.size() - w.size(); i++ )
+  {
+    if( isAnagram( w, s.substr( i, w.size() ) ) ) indecies.push_back( i );
+  }
+  return indecies;
+}
--- src/test_Problems_111_120.cpp
+++ src/test_Problems_111_120.cpp
@@ -2,3 +2,13 @@
 #include "Problem_112.hpp"
 
 #include "gtest/gtest.h"
+
+// Problem 111
+TEST( Problem_111, Given_Case )
+{
+  // given that W is "ab", and S is "abxaba", return 0, 3, and 4.
+  std::vector<int> expected = { 0, 3, 4 };
+  std::vector<int> result   = anagramIndex( "ab", "abxaba" );
+
+  EXPECT_EQ( expected, result );
+}
