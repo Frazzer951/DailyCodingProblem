964a0890fced039d74376cefa661075de7ef28f6
Luke Eltiste <luke343279@gmail.com>
2021-07-12 08:03:18 -0700

    add problems

---
+++ problems/Problem_116.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+/* MEDIUM
+Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+
+That is, generate() should return a tree whose size is unbounded but finite.
+*/
---
+++ problems/Problem_118.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+/* EASY
+Given a binary tree, return the level of the tree with minimum sum
+*/
