225ba299e1c6d37fc2846efd014006c5c2a529bd
Luke Eltiste <luke343279@gmail.com>
2021-07-29 08:53:50 -0700

    prob 131, skipped for now

--- problems/Problem_131.hpp
+++ problems/Problem_131.hpp
@@ -4,3 +4,42 @@
 Given the head to a singly linked list, where each node also has a “random” pointer that points to anywhere in the
 linked list, deep clone the list.
 */
+
+#include <map>
+
+#include "Futils.hpp"
+
+struct rslNode
+{
+  int       value;
+  rslNode * next;
+  rslNode * random;
+
+  rslNode( int x, rslNode * n = nullptr, rslNode * r = nullptr )
+  {
+    value  = x;
+    next   = n;
+    random = r;
+  }
+};
+inline std::ostream & operator<<( std::ostream & os, const rslNode & n )
+{
+  os << n.value;
+  if( n.random != nullptr ) { os << "\n\trandom: -> " << n.random->value; }
+  if( n.next != nullptr ) { os << "\n\tnext -> " << *n.next; }
+  return os;
+}
+inline bool operator==( const rslNode & lhs, const rslNode & rhs )
+{
+  if( lhs.value != rhs.value ) return false;
+  if( lhs.next != nullptr && rhs.next == nullptr ) return false;
+  if( lhs.next == nullptr && rhs.next != nullptr ) return false;
+  if( lhs.next != nullptr && rhs.next != nullptr )
+    if( !( *lhs.next == *rhs.next ) ) return false;
+  if( lhs.random != nullptr && rhs.random == nullptr ) return false;
+  if( lhs.random == nullptr && rhs.random != nullptr ) return false;
+  if( lhs.random != nullptr && rhs.random != nullptr )
+    if( !( *lhs.random == *rhs.random ) ) return false;
+
+  return true;
+}
---
+++ src/test_Problems_131_140.cpp
@@ -0,0 +1,7 @@
+#include "Problem_131.hpp"
+#include "Problem_132.hpp"
+#include "Problem_133.hpp"
+#include "Problem_134.hpp"
+#include "Problem_135.hpp"
+
+#include "gtest/gtest.h"
