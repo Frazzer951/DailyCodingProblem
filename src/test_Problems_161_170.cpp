d8463efed67fef011fbc257451e1f9d0be37bc5d
Luke Eltiste <luke343279@gmail.com>
2021-08-23 09:03:31 -0700

    problem 161

--- src/test_Problems_161_170.cpp
+++ src/test_Problems_161_170.cpp
@@ -1,3 +1,13 @@
 #include "Problem_161.hpp"
 
 #include "gtest/gtest.h"
+
+// Problem 161
+TEST( Problem_161, Given_Case )
+{
+  // given 1111 0000 1111 0000 1111 0000 1111 0000, return 0000 1111 0000 1111 0000 1111 0000 1111
+  long int x      = 0xF0F0F0F0;
+  long int result = reverse( x );
+
+  EXPECT_EQ( result, 0x0F0F0F0F );
+}
