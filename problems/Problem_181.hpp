45dd40848705ae0695882a740a8d2e44489b3224
Luke Eltiste <luke343279@gmail.com>
2021-09-13 09:17:09 -0700

    Problem 181

--- problems/Problem_181.hpp
+++ problems/Problem_181.hpp
@@ -7,3 +7,26 @@ For example, given the input string racecarannakayak, return ["racecar", "anna",
 
 Given the input string abc, return ["a", "b", "c"].
 */
+
+#include <string>
+#include <vector>
+
+#include "Problem_034.hpp"
+
+inline std::vector<std::string> splitPalindrome( std::string str )
+{
+  std::vector<std::string> palindromes;
+
+  while(!str.empty()){
+    for(int i = str.size(); i > 0; i--){
+      std::string substr = str.substr(0,i);
+      if(isPalindrome(substr)){
+        palindromes.push_back(substr);
+        str = str.substr(i);
+        break;
+      }
+    }
+  }
+
+  return palindromes;
+}
--- src/test_Problems_181_190.cpp
+++ src/test_Problems_181_190.cpp
@@ -1,3 +1,24 @@
 #include "Problem_181.hpp"
 
 #include "gtest/gtest.h"
+
+// Problem 181
+TEST( Problem_181, Given_Case_1 )
+{
+  // Given the input string racecarannakayak, return ["racecar", "anna", "kayak"].
+
+  std::vector<std::string> result   = splitPalindrome( "racecarannakayak" );
+  std::vector<std::string> expected = { "racecar", "anna", "kayak" };
+
+  EXPECT_EQ( result, expected );
+}
+
+TEST( Problem_181, Given_Case_2 )
+{
+  // Given the input string abc, return ["a", "b", "c"].
+
+  std::vector<std::string> result   = splitPalindrome( "abc" );
+  std::vector<std::string> expected = { "a", "b", "c" };
+
+  EXPECT_EQ( result, expected );
+}
