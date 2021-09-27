bdeb4cc87d29c32fdcdc894b1a04bf14830e1e50
Luke Eltiste <luke343279@gmail.com>
2021-09-27 14:48:58 -0700

    fix formating

--- problems/Problem_196.hpp
+++ problems/Problem_196.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 /* EASY
-Given the root of a binary tree, find the most frequent subtree sum. The subtree sum of a node is the sum of all values under a node, including the node itself.
+Given the root of a binary tree, find the most frequent subtree sum. The subtree sum of a node is the sum of all values
+under a node, including the node itself.
 
 For example, given the following tree:
 
--- src/test_Problems_191_200.cpp
+++ src/test_Problems_191_200.cpp
@@ -1,5 +1,8 @@
 #include "Problem_191.hpp"
 #include "Problem_192.hpp"
 #include "Problem_193.hpp"
+#include "Problem_194.hpp"
+#include "Problem_195.hpp"
+#include "Problem_196.hpp"
 
 #include "gtest/gtest.h"
