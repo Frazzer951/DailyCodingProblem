5897f7d8e01c9bba3ea43d777481a41c58130b13
Frazzer951 <Frazzer951@users.noreply.github.com>
2021-11-19 23:49:30 +0000

    Clang Tidy And Format

--- problems/include/problems_241_250/Problem_248.hpp
+++ problems/include/problems_241_250/Problem_248.hpp
@@ -1,6 +1,11 @@
+#ifndef PROBLEMS_241_250_PROBLEM_248_HPP
+#define PROBLEMS_241_250_PROBLEM_248_HPP
+
 #pragma once
 
 /* HARD
 Find the maximum of two numbers without using any if-else statements, branching,
 or direct comparisons.
 */
+
+#endif
