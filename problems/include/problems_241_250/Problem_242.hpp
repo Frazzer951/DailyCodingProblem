f58c0e4c797ed3c3a836e0b92d6c0cb13c00b686
Luke Eltiste <luke343279@gmail.com>
2021-11-29 11:41:17 -0800

    Problem 242

--- problems/include/problems_241_250/Problem_242.hpp
+++ problems/include/problems_241_250/Problem_242.hpp
@@ -16,4 +16,18 @@ You can assume that all values get cleared at the end of the day, and that you
 will not be asked for start and end values that wrap around midnight.
 */
 
+class sub_hour_counter
+{
+  int subs[24] = { 0 };
+
+public:
+  void update( int hour, int value ) { subs[hour] += value; }
+  int  query( int start, int end )
+  {
+    int sum = 0;
+    for( int i = start; i <= end; i++ ) sum += subs[i];
+    return sum;
+  }
+};
+
 #endif
--- src/test_Problems_241_250.cpp
+++ src/test_Problems_241_250.cpp
@@ -12,7 +12,7 @@
 #include "problems_241_250/Problem_250.hpp"
 
 // Problem 241
-TEST( Problem_241, GivenCase )
+TEST( Problem_241, Given_Case )
 {
   // N = 5, and the respective citations of each paper are [4, 3, 0, 1, 5]. Then the h-index would be 3
   int arr[]  = { 4, 3, 0, 1, 5 };
@@ -21,7 +21,19 @@ TEST( Problem_241, GivenCase )
 }
 
 // Problem 242
-
+TEST( Problem_242, Test_Case )
+{
+  sub_hour_counter counter;
+  EXPECT_EQ( 0, counter.query( 0, 23 ) );
+  for( int i = 0; i < 24; i++ ) { counter.update( i, i * 10 ); }
+  for( int i = 0; i < 24; i++ ) { EXPECT_EQ( i * 10, counter.query( i, i ) ); }
+  int sum = 0;
+  for( int i = 0; i < 24; i++ )
+  {
+    sum += i * 10;
+    EXPECT_EQ( sum, counter.query( 0, i ) );
+  }
+}
 
 // Problem 243
 
